ac1fd667f348070efa380e8a09719649d358b7b2
zhqmrz <m15855183250@163.com>
2024-12-03 14:32:36 +0800

    hello

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -11,7 +11,7 @@ if [[ -n "$jar_file" && -f "$jar_file" ]]; then
   echo -e "\nStarting Signal-Server using $jar_file\n"
   # Export the environmental variables when starting the server instead of keeping them in .bashrc
   source personal-config/secrets.env
-  sudo docker-compose up -d
+  sudo docker compose up -d
   # Sleep so that the cluster will be reachable by the time Signal-Server attempts to connect
   sleep 4
   # Start the server with the selected JAR file and configuration, and filter the output
@@ -26,7 +26,7 @@ if [[ -n "$jar_file" && -f "$jar_file" ]]; then
   }'
 else
   echo -e "\nNo valid Signal-Server JAR file found." # Else echo that the server couldn't be found -ChatGPT
-  sudo docker-compose down
+  sudo docker compose down
   cd scripts
   sleep 2
   exit
@@ -42,21 +42,21 @@ done
 
 echo -e "\nStopped $jar_file\n"
 
-read -p "Do you want to stop docker-compose.yml? (Press Enter to continue type 'n' to exit): " choice
+read -p "Do you want to stop docker compose.yml? (Press Enter to continue type 'n' to exit): " choice
 
 if [[ $choice == "n" ]]; then
   echo -e "\nExiting..."
 else
-  sudo docker-compose down
+  sudo docker compose down
   cd scripts
-  echo -e "\nStopped docker-compose dependencies"
+  echo -e "\nStopped docker compose dependencies"
 fi
 
 # Here is a stripped down version of this script in case it fails (enter one-by-one or place this script in `Signal-Server`)
 #
 # source personal-config/secrets.sh
-# sudo docker-compose down
-# sudo docker-compose up -d
+# sudo docker compose down
+# sudo docker compose up -d
 # sleep 4
 # java -jar -Dsecrets.bundle.filename=personal-config/config-secrets-bundle.yml service/target/your.jar server personal-config/config.yml
 #
